5582414131a74772e9999fa49180f337e259d896
Oumardev <oumsdev@gmail.com>
2022-06-07 13:16:47 +0000

    fix and feat

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
     production : {
       user : process.env.USER_SERVER,
       host : "86.107.197.161",
-      ref  : 'origin/master',
+      ref  : 'origin/ currentwork',
       repo : 'git@github.com:Oumardev/apoloan.backend.api.git',
       path : process.env.PATH_DEPLOY,
       'post-deploy' : 'npm install'
